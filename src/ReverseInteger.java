15adc158910dbaae1576183641774bf27414c3a1
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-13 21:11:41 +0530

    Submitted ReverseInteger

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,3 +1,13 @@
+import java.util.*;
 public class ReverseInteger { 
-        // TODO: Read an integer and print its reverse  
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int rev = 0;
+                while(n>0){
+                        rev = rev*10+n%10;
+                        n/=10;
+                }
+                System.out.println(rev);
+        }
 }
